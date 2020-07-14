ecfd2ed01493b437cf0f166bc0c09cc6805585eb
Razorwire007 <68270501+Razorwire007@users.noreply.github.com>
2020-07-14 13:31:38 +0530

    Created Project
    Project contains code to calculate radius of Circle and identify extension of the input file name.

---
+++ radius.py
@@ -0,0 +1,20 @@
+x = float(input("Enter Radius of Circle : "))  
+y = float(x**2*3.14) 
+print("Area of the circle is :" ,y )
+
+
+
+
+a = input("Enter File Name : ")
+py = "python"
+cpp = "C++"
+if "py" in a:
+    print("Python")
+
+elif "cpp" in a:
+    print("C++")
+
+else:
+    print("False")
+print("The Extension of the file is: ", a)
+
